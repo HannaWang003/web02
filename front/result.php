c8105d89c4f55b8d1c0022d02c59b848640a48eb
HannaWang003 <hannawang003@gmail.com>
2024-10-09 16:50:14 +0800

    完成p5

--- front/result.php
+++ front/result.php
@@ -8,17 +8,18 @@ $mids = $Que->all(['big_id' => $_GET['id']]);
     <table class="tab">
         <?php
         foreach ($mids as $idx => $mid) {
-            $bignum = ($big['vote'] != 0) ?? 1;
-            $width = floor($mid['vote'] / $bignum, 2);
+            $bignum = $big['vote'] ?: 1;
+            $width = round($mid['vote'] / $bignum, 2) * 100;
         ?>
-            <tr>
-                <td width="35%" style="padding:1%;"><?= $idx + 1 ?>.<?= $mid['text'] ?></td>
-                <td style="padding:3%;">
-                    <div style="height:30px;width:<?= $width ?>%;background:#0fe"></div>
-                </td>
-            </tr>
+        <tr>
+            <td width="35%" style="padding:1%;"><?= $idx + 1 ?>.<?= $mid['text'] ?></td>
+            <td style="padding:3%;">
+                <div style="height:30px;width:<?= $width ?>%;background:#0fe"></div><?= $mid['vote'] ?>票(<?= $width ?>%)
+            </td>
+        </tr>
         <?php
         }
         ?>
     </table>
+    <div class="ct"><button onclick="location.href='?do=que'">返回</button></div>
 </fieldset>

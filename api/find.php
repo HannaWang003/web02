c46398e176cdda3aa38b96f5ece8f0c500bc638c
HannaWang003 <hannawang003@gmail.com>
2024-10-10 14:56:44 +0800

    p7 init

--- api/find.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once "db.php";
-$res = $DB->find($_POST);
-if ($res != "") {
-    echo "您的密碼為 : <b>{$res['pw']}</b>";
-} else {
-    echo "<span style='color:red;'>查無此資料</span>";
-}
--- api/get_title.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "db.php";
-$rows = $DB->all($_POST);
-foreach ($rows as $row) {
-?>
-    <h4 onclick="getArticle(<?= $row['id'] ?>)"><?= $row['title'] ?></h4>
-<?php
-}
-?>

517e16ff22135e738cf5d3f1f79c4565ef095a27
HannaWang003 <hannawang003@gmail.com>
2024-10-16 19:06:49 +0800

    init p8

--- api/get_title.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "db.php";
-$rows = $DB->all($_POST);
-foreach ($rows as $row) {
-?>
-    <div class="clo" onclick="getArticle(<?= $row['id'] ?>)"><?= $row['title'] ?></div>
-    </p>
-<?php
-}
